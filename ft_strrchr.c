2dd38f69ff246fbfc9adc28a5994e15b6c8b6850
Andrea Lenzini <alenzini91@gmail.com>
2022-10-24 18:38:32 +0200

    modified ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,24 +6,24 @@
 /*   By: alenzini <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/14 23:50:54 by alenzini          #+#    #+#             */
-/*   Updated: 2022/10/14 23:52:25 by alenzini         ###   ########.fr       */
+/*   Updated: 2022/10/24 18:35:27 by alenzini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/*cerca un carattere in una stringa e restituisce
-l'indirizzo dell'ultimo che trova*/
+
 char	*ft_strrchr(const char *s, int c)
 {
-	const char	*ini;
-	int			i;
+	int	i;
 
-	ini = s;
-	i = ft_strlen(s);
-	s = (s + i);
-	while (*s != *ini && c != *s)
-		s--;
-	if (c == *s)
-		return ((char *)s);
-	return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	while (i >= 0)
+	{
+		if (s[i] == (char)c)
+			return ((char *)(s + i));
+		i--;
+	}
+	return (NULL);
 }
